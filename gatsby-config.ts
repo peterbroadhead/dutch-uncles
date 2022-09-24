9e367200456d5eb755ba274269cc7b920424cad6
Peter Broadhead <p.t.broadhead@gmail.com>
2022-09-24 11:17:39 +0100

    First commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Dutch Uncles`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `hhttps://dutchuncles.co.uk/`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
